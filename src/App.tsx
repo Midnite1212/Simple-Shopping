e7feb53b9ffdaf1667f512e6101801d7e785489e
Richie Wee <ricia1212@gmail.com>
2022-06-07 16:05:57 +0800

    Basic design of cards done, API integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+import Item from "./Item/Item";
+import { Wrapper } from "./App-stlye";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
-}
+
+  const getTotalItems = () => {};
+
+  const handleAddToCart = (clickedItem: CartItemType) => {};
+
+  const handleRemoveFromCart = () => {};
+
+  data ? console.log(data) : console.log("none");
+
+  if (isLoading) return <LinearProgress />;
+
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item: CartItemType) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
+};
 
 export default App;
